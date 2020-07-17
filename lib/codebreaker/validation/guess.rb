09ec7c1facdea38d07f36609cecfd2ed316509f5
Kosinskiy Andrey <kosinskiy.andrey@ukr.net>
2020-07-17 03:00:19 +0300

    fix guess valisation

--- lib/codebreaker/validation/guess.rb
+++ lib/codebreaker/validation/guess.rb
@@ -1,7 +1,15 @@
 module Validation
   class Guess
     def self.validation(input_value:)
-      (input_value.to_i.is_a? Integer) && input_value.size == 4 && input_value.to_i.digits.all? { |n| (1..6).cover?(n) }
+      return if (input_value.to_i.is_a? Integer) && input_value.size == 4 && number_in_range?(input_value)
+
+      raise ArgumentError, 'Guess - number, required, length - 4 digits, each digit is a number in the range 1-6'
+    end
+
+    private
+
+    def number_in_range?(input_value, range = (1..6))
+      input_value.to_i.digits.all? { |digit| range.cover?(digit) }
     end
   end
 end
