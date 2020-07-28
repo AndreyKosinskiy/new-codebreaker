aaddb9045c77083fce154676bed66ccb19409ab0
Kosinskiy Andrey <kosinskiy.andrey@ukr.net>
2020-07-28 23:54:23 +0300

    put variable to let block

--- spec/game/code_maker_spec.rb
+++ spec/game/code_maker_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe Game::CodeMaker do
   let(:code_first) { described_class.new }
   let(:code_second) { described_class.new }
+
   it 'ganerate random code' do
     expect(code_first.code).not_to eq(code_second.code)
   end
